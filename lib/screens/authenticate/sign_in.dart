649e0ecf6fe2e44124da2e4e6cc9e39078e45d5b
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-08-11 10:37:38 +0530

    Auth files

---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.redAccent[200],
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent[500],
+        
+      ),
+    );
+  }
+}
